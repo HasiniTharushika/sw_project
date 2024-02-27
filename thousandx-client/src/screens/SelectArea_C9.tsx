8b565e099522eaaa8079528b7b73346e898b8e31
HasiniTharushika <hasinitdw@gmail.com>
2024-02-27 13:41:58 +0530

    Reinstelled the tailwindcss

--- thousandx-client/src/screens/SelectArea_C9.tsx
+++ thousandx-client/src/screens/SelectArea_C9.tsx
@@ -1,124 +1,124 @@
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, TouchableWithoutFeedback, KeyboardAvoidingView } from 'react-native'
-import React, { useState } from 'react'
-import DropDownPicker from 'react-native-dropdown-picker';
-import { Colors } from '../utils/Colors';
-import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+// import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, TouchableWithoutFeedback, KeyboardAvoidingView } from 'react-native'
+// import React, { useState } from 'react'
+// import DropDownPicker from 'react-native-dropdown-picker';
+// import { Colors } from '../utils/Colors';
+// import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+// import AntDesign from 'react-native-vector-icons/AntDesign';
 
-const SelectArea_C9 = () => {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const [items, setItems] = useState([
-    {label: 'Colombo', value: 'colombo'},
-    {label: 'Gampaha', value: 'gampaha1'},
-    {label: 'Kalutara', value: 'gampaha2'},
-    {label: 'Kandy', value: 'gampaha3'},
-    {label: 'Kegalle', value: 'colombo1'},
-    {label: 'Matara', value: 'colombo2'},
-    {label: 'Colombo', value: 'colombo23'},
-    {label: 'Colombo', value: 'colombo3'},
-  ]);
-  return (
+// const SelectArea_C9 = () => {
+//   const [open, setOpen] = useState(false);
+//   const [value, setValue] = useState(null);
+//   const [items, setItems] = useState([
+//     {label: 'Colombo', value: 'colombo'},
+//     {label: 'Gampaha', value: 'gampaha1'},
+//     {label: 'Kalutara', value: 'gampaha2'},
+//     {label: 'Kandy', value: 'gampaha3'},
+//     {label: 'Kegalle', value: 'colombo1'},
+//     {label: 'Matara', value: 'colombo2'},
+//     {label: 'Colombo', value: 'colombo23'},
+//     {label: 'Colombo', value: 'colombo3'},
+//   ]);
+//   return (
     
-    <ScrollView >
+//     <ScrollView >
       
-    <View style={styles.container} >
-    <View style={styles.Icon}>
-          <TouchableOpacity >
-          <AntDesign name='left' size={30} color={Colors.PrimaryBlack}/>
-          </TouchableOpacity>
+//     <View style={styles.container} >
+//     <View style={styles.Icon}>
+//           <TouchableOpacity >
+//           <AntDesign name='left' size={30} color={Colors.PrimaryBlack}/>
+//           </TouchableOpacity>
           
-        </View>
-      <View>     
-         <Text style={styles.heading}>Select your area</Text>
+//         </View>
+//       <View>     
+//          <Text style={styles.heading}>Select your area</Text>
          
-        </View>
+//         </View>
 
         
-        <Image style={styles.img}
-            source={
-              require('../assets/appImages/3_location.png')
-            } />
+//         <Image style={styles.img}
+//             source={
+//               require('../assets/appImages/3_location.png')
+//             } />
        
-<DropDownPicker 
-style={{
+// <DropDownPicker 
+// style={{
 
-}}
+// }}
 
-  textStyle={{
-  fontSize: 15,
-  fontFamily:'Poppins'
-}}
-placeholder="Select your area"
-placeholderStyle={{
-  color: Colors.PrimaryGrey,
+//   textStyle={{
+//   fontSize: 15,
+//   fontFamily:'Poppins'
+// }}
+// placeholder="Select your area"
+// placeholderStyle={{
+//   color: Colors.PrimaryGrey,
  
-}}
-      min={1}
-      max={1}
-      open={open}
-      value={value}
-      items={items}
-      setOpen={setOpen}
-      setValue={setValue}
-      setItems={setItems}
-    />
-<TouchableOpacity>
-            <Text style={styles.buttonStyle}>Continue</Text>
-          </TouchableOpacity>
+// }}
+//       min={1}
+//       max={1}
+//       open={open}
+//       value={value}
+//       items={items}
+//       setOpen={setOpen}
+//       setValue={setValue}
+//       setItems={setItems}
+//     />
+// <TouchableOpacity>
+//             <Text style={styles.buttonStyle}>Continue</Text>
+//           </TouchableOpacity>
 
 
 
-    </View>
-    </ScrollView>
+//     </View>
+//     </ScrollView>
     
-  )
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    margin: 20,
-    paddingBottom:20,
-  },
-  Icon: {
-    marginBottom:30,
-  },
-  heading: {
-    fontSize: 15,
-    fontFamily: 'Poppins',
-    fontWeight: 'bold',
-    color: Colors.PrimaryBlack,
-  },
-  img: {
-    justifyContent: 'center',
-    alignContent: 'center',
-    width: 250,
-    height: 350,
+//   )
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // justifyContent: 'center',
+//     margin: 20,
+//     paddingBottom:20,
+//   },
+//   Icon: {
+//     marginBottom:30,
+//   },
+//   heading: {
+//     fontSize: 15,
+//     fontFamily: 'Poppins',
+//     fontWeight: 'bold',
+//     color: Colors.PrimaryBlack,
+//   },
+//   img: {
+//     justifyContent: 'center',
+//     alignContent: 'center',
+//     width: 250,
+//     height: 350,
   
-    // width:'100%',
-    // height:'100%',
-    resizeMode: "contain",
-},
-dropDownText:{
-  fontFamily:'Poppins',
-  fontSize:15,
-},
-buttonStyle: {
-  height: 40,
-  color: Colors.PrimaryWhite,
-  backgroundColor: Colors.PrimaryBlue,
-  borderRadius: 10,
-  textAlign: 'center',
-  fontFamily: 'Poppins',
-  fontWeight: 'bold',
-  marginTop: 10,
-  // marginHorizontal: 20,
-  lineHeight: 40,
-},
-})
+//     // width:'100%',
+//     // height:'100%',
+//     resizeMode: "contain",
+// },
+// dropDownText:{
+//   fontFamily:'Poppins',
+//   fontSize:15,
+// },
+// buttonStyle: {
+//   height: 40,
+//   color: Colors.PrimaryWhite,
+//   backgroundColor: Colors.PrimaryBlue,
+//   borderRadius: 10,
+//   textAlign: 'center',
+//   fontFamily: 'Poppins',
+//   fontWeight: 'bold',
+//   marginTop: 10,
+//   // marginHorizontal: 20,
+//   lineHeight: 40,
+// },
+// })
 
-export default SelectArea_C9
+// export default SelectArea_C9
 
 
 
